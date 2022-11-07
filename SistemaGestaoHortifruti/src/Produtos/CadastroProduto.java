335000f448b66de2b655e3d370839152ac99d9fe
Willian de Jesus Caetano <willian.caetano@mercadolivre.com>
2022-11-07 14:28:06 -0300

    Correções de erros no Scanner

--- SistemaGestaoHortifruti/src/Produtos/CadastroProduto.java
+++ SistemaGestaoHortifruti/src/Produtos/CadastroProduto.java
@@ -1,26 +1,8 @@
 package Produtos;
-import Navegacao.MenuNavegacao;
-import Produtos.Produtos;
-import Vendas.MenuVendas;
-
-import java.util.Scanner;
-
-import static Menu.Hortifruti.scanner;
 
 public class CadastroProduto {
-    Scanner sc = new Scanner(System.in);
+
     public void cadastrarProduto(){
-        String nome;
-        while (true){
-
-            System.out.println("===================================================================");
-            System.out.println("||                     CADASTRO DE PRODUTOS                      ||");
-            System.out.println("===================================================================");
-
-            System.out.print("Digite o nome do produto a ser cadastrado: ");
-            nome = sc.nextLine();
-
-
-        }
+        System.out.println("Produto Cadastrado!");
     }
 }
